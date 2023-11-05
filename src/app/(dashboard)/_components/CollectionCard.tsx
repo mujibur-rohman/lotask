331a397240d8d560ea2e993332c31e0a2c8eb452
mujiburrohman <muji.official06@gmail.com>
2023-11-05 10:01:28 +0700

    feat: list, delete, task

--- src/app/(dashboard)/_components/CollectionCard.tsx
+++ src/app/(dashboard)/_components/CollectionCard.tsx
@@ -29,8 +29,10 @@ import {
   TrashIcon,
 } from "lucide-react";
 import { useRouter } from "next/navigation";
-import React, { useState, useTransition } from "react";
+import React, { useMemo, useState, useTransition } from "react";
 import { toast } from "sonner";
+import CreateTaskDialog from "./CreateTaskDialog";
+import TaskCard from "./TaskCard";
 
 interface Props {
   collection: Collection & {
@@ -40,6 +42,7 @@ interface Props {
 
 function CollectionCard({ collection }: Props) {
   const [isOpen, setIsOpen] = useState(true);
+  const [showCreateModal, setShowCreateModal] = useState(false);
   const [isLoading, startTransition] = useTransition();
   const router = useRouter();
 
@@ -55,93 +58,110 @@ function CollectionCard({ collection }: Props) {
     }
   };
 
+  const tasksDone = useMemo(() => {
+    return collection.tasks.filter((task) => task.done).length;
+  }, [collection.tasks]);
+
+  const totalTasks = collection.tasks.length;
+
+  const progress = totalTasks === 0 ? 0 : (tasksDone / totalTasks) * 100;
+
   return (
-    <Collapsible open={isOpen} onOpenChange={setIsOpen}>
-      <CollapsibleTrigger asChild>
-        <Button
-          variant={"ghost"}
-          className={cn(
-            "flex w-full justify-between p-6",
-            isOpen && "rounded-b-none",
-            CollectionColors[collection.color as CollectionColor]
-          )}
-        >
-          <span className="text-white font-bold">{collection.name}</span>
-          {!isOpen && <ChevronDownIcon className="h-6 w-6" />}
-          {isOpen && <ChevronUpIcon className="h-6 w-6" />}
-        </Button>
-      </CollapsibleTrigger>
-      <CollapsibleContent className="flex rounded-b-md flex-col dark:bg-neutral-900 shadow-lg">
-        {tasks.length === 0 && (
+    <React.Fragment>
+      <CreateTaskDialog
+        open={showCreateModal}
+        setOpen={setShowCreateModal}
+        collection={collection}
+      />
+      <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+        <CollapsibleTrigger asChild>
           <Button
             variant={"ghost"}
-            className="flex items-center justify-center gap-1 p-8 py-12 rounded-none"
-            //   onClick={() => setShowCreateModal(true)}
+            className={cn(
+              "flex w-full justify-between p-6",
+              isOpen && "rounded-b-none",
+              CollectionColors[collection.color as CollectionColor]
+            )}
           >
-            <p>There are no tasks yet:</p>
-            <span
-              className={cn(
-                "text-sm bg-clip-text text-transparent",
-                CollectionColors[collection.color as CollectionColor]
-              )}
-            >
-              Create one
-            </span>
+            <span className="text-white font-bold">{collection.name}</span>
+            {!isOpen && <ChevronDownIcon className="h-6 w-6" />}
+            {isOpen && <ChevronUpIcon className="h-6 w-6" />}
           </Button>
-        )}
-
-        {tasks.length > 0 && (
-          <>
-            <Progress className="rounded-none" value={progress} />
-            <div className="p-4 gap-3 flex flex-col">
-              {tasks.map((task) => (
-                <div key={task.id}>Tasks</div>
-              ))}
-            </div>
-          </>
-        )}
-        <Separator />
-        <footer className="h-[40px] px-4 p-[2px] text-xs text-neutral-500 flex justify-between items-center ">
-          <p>Created at {collection.createdAt.toLocaleDateString("en-US")}</p>
-          {isLoading && <div>Deleting...</div>}
-          {!isLoading && (
-            <div>
-              <Button
-                size={"icon"}
-                variant={"ghost"}
-                //   onClick={() => setShowCreateModal(true)}
+        </CollapsibleTrigger>
+        <CollapsibleContent className="flex rounded-b-md flex-col dark:bg-neutral-900 shadow-lg">
+          {tasks.length === 0 && (
+            <Button
+              variant={"ghost"}
+              className="flex items-center justify-center gap-1 p-8 py-12 rounded-none"
+              onClick={() => setShowCreateModal(true)}
+            >
+              <p>There are no tasks yet:</p>
+              <span
+                className={cn(
+                  "text-sm bg-clip-text text-transparent",
+                  CollectionColors[collection.color as CollectionColor]
+                )}
               >
-                <PlusIcon />
-              </Button>
-              <AlertDialog>
-                <AlertDialogTrigger asChild>
-                  <Button size={"icon"} variant={"ghost"}>
-                    <TrashIcon />
-                  </Button>
-                </AlertDialogTrigger>
-                <AlertDialogContent>
-                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    This action cannot be undone. This will permanently delete
-                    your collection and all tasks inside it.
-                  </AlertDialogDescription>
-                  <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction
-                      onClick={() => {
-                        startTransition(removeCollection);
-                      }}
-                    >
-                      Proceed
-                    </AlertDialogAction>
-                  </AlertDialogFooter>
-                </AlertDialogContent>
-              </AlertDialog>
-            </div>
+                Create one
+              </span>
+            </Button>
           )}
-        </footer>
-      </CollapsibleContent>
-    </Collapsible>
+
+          {tasks.length > 0 && (
+            <>
+              <Progress className="rounded-none" value={progress} />
+              <div className="p-4 gap-3 flex flex-col">
+                {tasks.map((task) => (
+                  <TaskCard key={task.id} task={task} />
+                ))}
+              </div>
+            </>
+          )}
+          <Separator />
+          <footer className="h-[40px] px-4 p-[2px] text-xs text-neutral-500 flex justify-between items-center ">
+            <p>Created at {collection.createdAt.toLocaleDateString("en-US")}</p>
+            {isLoading && <div>Deleting...</div>}
+            {!isLoading && (
+              <div>
+                <Button
+                  size={"icon"}
+                  variant={"ghost"}
+                  onClick={() => setShowCreateModal(true)}
+                >
+                  <PlusIcon />
+                </Button>
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <Button size={"icon"} variant={"ghost"}>
+                      <TrashIcon />
+                    </Button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogTitle>
+                      Are you absolutely sure?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                      This action cannot be undone. This will permanently delete
+                      your collection and all tasks inside it.
+                    </AlertDialogDescription>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>Cancel</AlertDialogCancel>
+                      <AlertDialogAction
+                        onClick={() => {
+                          startTransition(removeCollection);
+                        }}
+                      >
+                        Proceed
+                      </AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+              </div>
+            )}
+          </footer>
+        </CollapsibleContent>
+      </Collapsible>
+    </React.Fragment>
   );
 }
 
